fbfbe95bdb0c0b527e3e6d0e9e17ed6fda2ff751
raghunathliku <93243476+raghunathliku@users.noreply.github.com>
2024-10-20 17:41:18 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("helllo my first line of code1");
+        System.out.println("my second line of code2");
+        System.out.println(new Date().toString());
+    }
+}
